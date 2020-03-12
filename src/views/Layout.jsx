2f0f038507c3025a529f00c8da0c2553a9ff55f4
Hanzen Lim <hanzen@laguro.com>
2020-03-12 17:43:09 -0400

    :recycle: Added proptype check in Layout.jsx

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Layout = ({
-                  pageName, menu, children
-                }) => {
-  return (
-    <main className={`${pageName} layout`}>
-      <nav>
-        { menu }
-      </nav>
+const Layout = ({ pageName, menu, children }) => (
+  <main className={`${pageName} layout`}>
+    <nav>{menu}</nav>
 
-      <section>
-        { children }
-      </section>
-    </main>
-  );
+    <section>{children}</section>
+  </main>
+);
+
+Layout.propTypes = {
+  pageName: PropTypes.string.isRequired,
+  menu: PropTypes.func.isRequired,
+  children: PropTypes.func.isRequired
 };
 
 export default Layout;
