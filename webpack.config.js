01890725090cb15fd7c2f161ad7be31789f09c89
Hanzen Lim <hanzen@laguro.com>
2020-03-13 11:03:50 -0400

    :wrench: Update webpack to development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 "use strict";
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const DashboardPlugin = require('webpack-dashboard/plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DashboardPlugin = require("webpack-dashboard/plugin");
 
 // const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // These files will be imported in every sass file
@@ -13,79 +13,79 @@ const DashboardPlugin = require('webpack-dashboard/plugin');
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || "7357";
 
-
 module.exports = {
   entry: [
     // POLYFILL: Set up an ES6-ish environment
     // 'babel-polyfill',  // The entire babel-polyfill
     // Or pick es6 features needed (included into babel-polyfill)
-    'core-js/fn/promise',
-    'core-js/es6/object',
-    'core-js/es6/array',
+    "core-js/fn/promise",
+    "core-js/es6/object",
+    "core-js/es6/array",
 
-    './src/index.jsx', // your app's entry point
+    "./src/index.jsx" // your app's entry point
   ],
-  devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
+  devtool: process.env.WEBPACK_DEVTOOL || "eval-source-map",
   output: {
-    publicPath: '/',
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    publicPath: "/",
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
   },
+  mode: "development",
   module: {
     rules: [
-      { // handle images
+      {
+        // handle images
         test: /\.(png|svg|jpg|gif)$/,
         use: {
           loader: "file-loader"
         }
       },
-      { // handles css in legacy app
+      {
+        // handles css in legacy app
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"]
       },
-      { // handle sass
+      {
+        // handle sass
         test: /\.sass$/,
         use: [
           "style-loader",
           "css-loader",
-          "sass-loader",
+          "sass-loader"
           // {
           //   loader: "sass-resources-loader",
           //   options: {
           //     resources: sassResourcesPaths
           //   }
           // }
-
         ]
       },
-      { // process index alone as application root
+      {
+        // process index alone as application root
         test: /\.html$/,
         use: "html-loader"
       },
-      { // handles angular templates referenced in js
+      {
+        // handles angular templates referenced in js
         test: /\.json/,
         include: path.resolve(__dirname, "fixtures/mocks"),
-        use: [
-          'file-loader'
-        ]
+        use: ["file-loader"]
       },
       {
         test: /\.(js|jsx)$/,
         include: path.resolve(__dirname, "src"),
-        use: 'babel-loader'
+        use: "babel-loader"
       }
     ]
-  },  resolve: {
-    extensions: ['.js', '.jsx'],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
     modules: [
       path.join(__dirname, "src"),
-      path.join(__dirname, "node_modules"), // the old 'fallback' option (needed for npm link-ed packages)
+      path.join(__dirname, "node_modules") // the old 'fallback' option (needed for npm link-ed packages)
     ],
     alias: {
-      "styles": path.resolve(__dirname, 'styles/'),
+      styles: path.resolve(__dirname, "styles/")
     }
   },
   devServer: {
@@ -108,11 +108,11 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new DashboardPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/template.html',
+      template: "./src/template.html",
       files: {
-        css: ['style.css'],
-        js: [ "bundle.js"],
+        css: ["style.css"],
+        js: ["bundle.js"]
       }
-    }),
+    })
   ]
 };
