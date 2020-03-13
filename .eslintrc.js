094c76f45e8b289f1d8ee09097683b2b54fad94d
Hanzen Lim <hanzen@laguro.com>
2020-03-13 12:01:47 -0400

    :wrench: Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "commonjs": true
+  env: {
+    es6: true,
+    commonjs: true
   },
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaVersion": 9,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
     }
   },
-  "extends": ["airbnb", "prettier","prettier/react"],
-  "plugins": [
-    "react",
-  ],
-  "rules": {
+  extends: ["airbnb", "prettier", "prettier/react"],
+  plugins: ["react"],
+  rules: {
     "spaced-comment": 0,
     "no-underscore-dangle": 0,
     "trailing-comma": 0
